5124cf3644582753a2c49c75e14f40ea78748370
Ricardo Quesada <ricardoquesada@gmail.com>
2012-08-20 17:59:27 -0700

    Regenerated cocos2d bindings with CCPhysicsSprite and CCPhysicsDebugNode

--- configs/cocos2d/gen_cocos2d_bridgesupport.sh
+++ configs/cocos2d/gen_cocos2d_bridgesupport.sh
@@ -5,7 +5,7 @@
 #
 mv ccDeprecated.h ccDeprecated.xxx
 
-gen_bridge_metadata -F complete --no-64-bit -c '-ISupport -IPlatforms -I.' *.h Support/*.h Platforms/*.h -o ../external/jsbindings/configs/cocos2d/cocos2d.bridgesupport 
+gen_bridge_metadata -F complete --no-64-bit -c '-DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -ISupport -IPlatforms -I. -I../external/Chipmunk/include/chipmunk/. -I../external/Chipmunk/include/chipmunk/constraints/.' *.h Support/*.h Platforms/*.h -o ../external/jsbindings/configs/cocos2d/cocos2d.bridgesupport
 
 gen_bridge_metadata -F complete --64-bit -c '-D__CC_PLATFORM_MAC -ISupport -IPlatforms -IPlatforms/Mac -I.' *.h Platforms/*.h Platforms/Mac/*.h -o ../external/jsbindings/configs/cocos2d/cocos2d-mac.bridgesupport 
 
